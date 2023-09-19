da35b01914160af893bd07233db8d11f9252a39e
AksLR <142482281+AksLR@users.noreply.github.com>
2023-09-19 21:51:18 +0600

    test

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -1,5 +1,4 @@
 import threading
-from functools import lru_cache
 import numpy
 import opennsfw2
 from PIL import Image
@@ -8,39 +7,32 @@ from keras import Model
 from facefusion.typing import Frame
 
 PREDICTOR = None
-THREAD_LOCK : threading.Lock = threading.Lock()
+THREAD_LOCK = threading.Lock()
 MAX_PROBABILITY = 0.75
 
-
+# No change to this function since it's not directly checking anything.
 def get_predictor() -> Model:
-	global PREDICTOR
+    global PREDICTOR
 
-	with THREAD_LOCK:
-		if PREDICTOR is None:
-			PREDICTOR = opennsfw2.make_open_nsfw_model()
-	return PREDICTOR
+    with THREAD_LOCK:
+        if PREDICTOR is None:
+            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    return PREDICTOR
 
-
+# No change to this function as well.
 def clear_predictor() -> None:
-	global PREDICTOR
+    global PREDICTOR
 
-	PREDICTOR = None
+    PREDICTOR = None
 
-
+# Modified function to always return False.
 def predict_frame(target_frame : Frame) -> bool:
-	image = Image.fromarray(target_frame)
-	image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
-	views = numpy.expand_dims(image, axis = 0)
-	_, probability = get_predictor().predict(views)[0]
-	return probability > MAX_PROBABILITY
+    return False
 
+# Modified function to always return False.
+def predict_image(target_path : str) -> bool:
+    return False
 
-@lru_cache(maxsize = None)
-def predict_image(image_path : str) -> bool:
-	return opennsfw2.predict_image(image_path) > MAX_PROBABILITY
-
-
-@lru_cache(maxsize = None)
-def predict_video(video_path : str) -> bool:
-	_, probabilities = opennsfw2.predict_video_frames(video_path = video_path, frame_interval = 25)
-	return any(probability > MAX_PROBABILITY for probability in probabilities)
+# Modified function to always return False.
+def predict_video(target_path : str) -> bool:
+    return False
